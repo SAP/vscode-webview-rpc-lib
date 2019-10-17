ec164b8ae16d393478587cb936ffbb1abe8c8e54
Eyal Barlev <eyal.barlev@sap.com>
2019-10-17 18:39:23 +0300

    added websocket-based implementation for development and test purposes

---
+++ src/rpc-browser-ws.ts
@@ -0,0 +1,56 @@
+// must specify ".js" for import in browser to locate rpc-common.js
+// see: https://github.com/microsoft/TypeScript/issues/16577#issuecomment-343610106
+
+import { RpcCommon, IPromiseCallbacks } from './rpc-common.js';
+
+export class RpcBrowserWebSockets extends RpcCommon {
+  ws: WebSocket;
+
+  constructor(ws: WebSocket) {
+    super();
+    this.ws = ws;
+    this.ws.addEventListener('message', (event) => {
+      const message: any = JSON.parse(event.data as string);
+      switch (message.command) {
+        case "rpc-response":
+          this.handleResponse(message);
+          break;
+        case 'rpc-request':
+          this.handleRequest(message);
+          break;
+      }
+    });
+  }
+
+  sendRequest(id: number, method: string, params: any[]) {
+    // TODO: consider cancelling the timer if the promise if fulfilled before timeout is reached
+    setTimeout(() => {
+      const promiseCallbacks: IPromiseCallbacks | undefined = this.promiseCallbacks.get(id);
+      if (promiseCallbacks) {
+        promiseCallbacks.reject("Request timed out");
+        this.promiseCallbacks.delete(id);
+      }
+    }, this.timeout);
+
+    // TODO: find an alternative to appending vscode to the global scope (perhaps providing vscode as parameter to constructor)
+    const requestBody: any = {
+      command: 'rpc-request',
+      id: id,
+      method: method,
+      params: params
+    };
+
+    this.ws.send(JSON.stringify(requestBody));
+  }
+
+  sendResponse(id: number, response: any, success: boolean = true): void {
+    const responseBody: any = {
+      command: 'rpc-response',
+      id: id,
+      response: response,
+      success: success
+    };
+
+    this.ws.send(JSON.stringify(responseBody));
+  }
+}
