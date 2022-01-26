73faa7aae68a0c5926d88aa3a11cb5bd142aa61f
ChayaLau <chaya.lau@sap.com>
2022-01-26 08:52:49 +0200

    added missing soffix .js of import noop logger, to fix running in browser issue

--- src/rpc-browser.ts
+++ src/rpc-browser.ts
@@ -5,7 +5,7 @@
 import { RpcCommon, IPromiseCallbacks } from "./rpc-common.js";
 import { Webview } from "vscode";
 import { IChildLogger } from "@vscode-logging/types";
-import { noopLogger } from "./noop-logger";
+import { noopLogger } from "./noop-logger.js";
 
 export class RpcBrowser extends RpcCommon {
   private static readonly className = "RpcBrowser";
